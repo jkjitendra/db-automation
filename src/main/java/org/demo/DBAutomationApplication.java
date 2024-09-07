44e4276f86cfb4f613b5b9a47285c6f6f23ab81a
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-09-08 00:13:58 +0530

    Configured Maven dependencies and plugins for Spring Boot, MariaDB, JPA, and Jasypt Initialized Spring Boot application for DB automation process

---
+++ src/main/java/org/demo/DBAutomationApplication.java
@@ -0,0 +1,15 @@
+package org.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBAutomationApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DBAutomationApplication.class, args);
+        
+    }
+
+}
